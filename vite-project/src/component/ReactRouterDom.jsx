0b09130af3e1904027624b567e8913897eb20261
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-12-30 21:45:42 +0600

    HashRouter

--- vite-project/src/component/ReactRouterDom.jsx
+++ vite-project/src/component/ReactRouterDom.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './../pages/HomePage';
 import ProductPage from './../pages/ProductPage';
 import ProfilePage from './../pages/ProfilePage';
 import NotFound from './../pages/NotFound';
 
 const ReactRouterDom = () => {
+    // return (
+    //   <div>
+    //     <BrowserRouter>
+    //       <Routes>
+    //         <Route path='/' element={<HomePage/>} />
+    //         <Route path='/product' element={<ProductPage/>} />
+    //         <Route path='/profile' element={<ProfilePage/>} />
+    //         <Route path='*' element={<NotFound/>} />
+    //       </Routes>
+    //     </BrowserRouter>
+    //   </div>
+    // );
+
     return (
       <div>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path='/' element={<HomePage/>} />
             <Route path='/product' element={<ProductPage/>} />
             <Route path='/profile' element={<ProfilePage/>} />
             <Route path='*' element={<NotFound/>} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
 };
